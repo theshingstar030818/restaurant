994db588679be6a0e708c6e58dffda4c99c138a2
tanzeelrana <tanzeelrana@live.com>
2017-02-21 04:13:08 -0500

    testing travis ci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import { Nav, Platform, Events } from 'ionic-angular';
+import { StatusBar, Splashscreen} from 'ionic-native';
 
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
